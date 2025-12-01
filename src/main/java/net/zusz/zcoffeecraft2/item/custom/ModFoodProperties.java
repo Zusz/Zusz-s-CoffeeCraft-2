e79d8823c24f885a4b1122824d81c6dbaf05a929
Zusz <zusz2011@gmail.com>
2025-12-01 11:40:30 +0100

    consuming coffees (and hot chocolate) now gives players the coffee cup item back

--- src/main/java/net/zusz/zcoffeecraft2/item/custom/ModFoodProperties.java
+++ src/main/java/net/zusz/zcoffeecraft2/item/custom/ModFoodProperties.java
@@ -1,11 +1,13 @@
 package net.zusz.zcoffeecraft2.item.custom;
 
 import net.minecraft.world.food.FoodProperties;
+import net.zusz.zcoffeecraft2.item.ModItems;
 
 public class ModFoodProperties {
     public static final FoodProperties CUP_OF_COFFEE = new FoodProperties.Builder()
             .nutrition(2)
             .saturationModifier(0.5f)
             .alwaysEdible()
+            .usingConvertsTo(ModItems.COFFEE_CUP)
             .build();
 }
